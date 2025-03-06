3e4e224dd70374370361d9b12d6d24d5a10bfb96
binnuuuu <binnuparupalli1@gmail.com>
2025-03-06 16:19:45 +0530

    Update org.asm

--- PHASE_2/org.asm
+++ PHASE_2/org.asm
@@ -1,1 +1,7 @@
-
+.text
+addi x1, x0, 5       # x1 = 5
+addi x2, x0, 10      # x2 = 10
+add x3, x1, x2       # Add x1 and x2, store result in x3
+sw x3, 0(x4)         # Store result of addition (x3) into address in x4
+addi x4, x0, 20      # x4 = 20
+lw x5, 0(x4)         # Load word from address in x4
