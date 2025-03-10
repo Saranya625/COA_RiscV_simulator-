3d4f7022137d8605f7d3ae46268519509fdc5fd2
Saranya <cs23b031@iittp.ac.in>
2025-03-10 05:43:20 +0530

    Update org.asm

--- PHASE_2/org.asm
+++ PHASE_2/org.asm
@@ -1,7 +1,11 @@
 .text
-addi x1, x0, 5       # x1 = 5
-addi x2, x0, 10      # x2 = 10
-add x3, x1, x2       # Add x1 and x2, store result in x3
-sw x3, 0(x4)         # Store result of addition (x3) into address in x4
-addi x4, x0, 20      # x4 = 20
-lw x5, 0(x4)         # Load word from address in x4
+main:
+add x2, x3 , x4 
+bne cid , 1 , label 
+sub x5, x6 , x7 
+addi x12,x0,12
+j exit
+label:
+mul x8, x9, x10
+exit:
+ecall
